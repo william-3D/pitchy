1ec81ea899a245d15c66f5804d04b98ffc57b4b2
Sonia William <98wsonia@gmail.com>
2025-03-02 18:54:27 +0800

    integrate OAuth for Github social login

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { auth } from '@/auth';
+import { auth, signOut, signIn } from '@/auth';
 
 const Navbar = async () => {
+    const session = await auth();
+
     return (
         <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
             <nav className='flex justify-between items-center'>
@@ -11,8 +13,38 @@ const Navbar = async () => {
                     <Image src='/logo.png' alt='Logo' width={100} height={30} />
                 </Link>
 
-                <div className="flex items-center gap-5">
-                    {/* if logged in */}
+                <div className='flex items-center gap-5'>
+                    {session && session?.user ? (
+                        <>
+                            <Link href='/pitch/create'>
+                                <span>Pitch</span>
+                            </Link>
+
+                            <form
+                                action={async () => {
+                                    'use server';
+                                    await signOut({ redirectTo: '/' });
+                                }}
+                            >
+                                <button type='submit'>Log out</button>
+                            </form>
+
+                            <Link href={`/user/${session?.id}`}>
+                                <span>{session?.user?.name}</span>
+                            </Link>
+                        </>
+                    ) : (
+                        <form
+                            action={async () => {
+                                'use server';
+                                await signIn('github');
+                            }}
+                        >
+                            <button className='text-black' type='submit'>
+                                Login
+                            </button>
+                        </form>
+                    )}
                 </div>
             </nav>
         </header>
