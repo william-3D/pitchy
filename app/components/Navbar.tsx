b22b1d6e7d3d64761df2d53856e40e02eb3a1101
Sonia William <98wsonia@gmail.com>
2025-03-02 18:27:50 +0800

    add navbar UI

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const Navbar = () => {
-    return <div>Navbar</div>;
+import { auth } from '@/auth';
+
+const Navbar = async () => {
+    return (
+        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className='flex justify-between items-center'>
+                <Link href='/'>
+                    <Image src='/logo.png' alt='Logo' width={100} height={30} />
+                </Link>
+
+                <div className="flex items-center gap-5">
+                    {/* if logged in */}
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Navbar;
