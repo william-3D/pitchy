5d4b7deef7d07aba569f827419cc3ba1fdfc1f8b
Sonia William <98wsonia@gmail.com>
2025-03-03 21:14:15 +0800

    configure project colours & tailwind plugins

---
+++ tailwind.config.ts
@@ -0,0 +1,51 @@
+import type {Config} from "tailwindcss";
+
+const config: Config = {
+    darkMode: ["class"],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./sanity/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                xs: "475px",
+            },
+            colors: {
+                primary: {
+                    "100": "#7dc0fa",
+                    DEFAULT: "#0171d3",
+                },
+                secondary: "#c9fd06",
+                black: {
+                    "100": "#333333",
+                    "200": "#141413",
+                    "300": "#7D8087",
+                    DEFAULT: "#000000",
+                },
+                white: {
+                    "100": "#F7F7F7",
+                    DEFAULT: "#FFFFFF",
+                },
+            },
+            fontFamily: {
+                "work-sans": ["var(--font-work-sans)"],
+            },
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)",
+            },
+            boxShadow: {
+                100: "2px 2px 0px 0px rgb(0, 0, 0)",
+                200: "2px 2px 0px 2px rgb(0, 0, 0)",
+                300: "2px 2px 0px 2px rgb(1, 113, 211)",
+            },
+        },
+    },
+    plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
+
+export default config;
